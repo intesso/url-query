e18ca1096165e1c6066620dfa6deea512ea0da11
andi-neck <andi.neck@intesso.com>
2015-05-18 17:00:36 +0200

    adds object-equal for tests

--- test.js
+++ test.js
@@ -2,14 +2,7 @@ var assert = require('assert');
 
 var query = require('./index'), param;
 
-function equals(a, b) {
-  if (!a && !b) return true;
-  if (!a || !b) return false;
-
-  return Object.keys(a).every(function(key) {
-    return a[key] === b[key];
-  });
-}
+var equals = require('object-equal');
 
 assert(equals(query('?a=b'), {a:'b'}));
 assert(equals(query('?suuper=star&caret=rocks'), {caret:'rocks', suuper:'star'}));
